66fdfbad5345827032da8f93f90171e3c59202fc
yjoh9302 <ohyj48@naver.com>
2018-05-15 10:22:40 +0900

    DogLove2018-05-15 title Bulma

--- commit_push.bat
+++ commit_push.bat
@@ -1,5 +1,5 @@
 git add --all
 
-git commit -m "DogLove2018-05-15"
+git commit -m "DogLove2018-05-15 title Bulma"
 
 git push -u origin master
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="section">
-    <div class="content">
+    <div class="hero is-danger">
+      <div calss="hero-body">
       <h1 class="title is-1">개사랑</h1>
       <h2 class="subtitle">개를 좋아하는 사람들</h2>
+    </div>
     </div>
 
   </section>
