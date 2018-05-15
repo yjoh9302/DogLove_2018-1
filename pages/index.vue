860420f11cb2ecd77c1165e0d6998d455b531526
yjoh9302 <ohyj48@naver.com>
2018-05-15 10:17:30 +0900

    DogLove2018-05-15

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="content">
-      <h1 class="title">개사랑</h1>
+      <h1 class="title is-1">개사랑</h1>
       <h2 class="subtitle">개를 좋아하는 사람들</h2>
     </div>
 
