b30ddbadc767268b3f4fbc3e5299c8848e3d4053
yjoh9302 <ohyj48@naver.com>
2018-05-15 10:45:34 +0900

    DogLove2018-05-15 title Bulma article

--- commit_push.bat
+++ commit_push.bat
@@ -1,5 +1,5 @@
 git add --all
 
-git commit -m "DogLove2018-05-15 title Bulma"
+git commit -m "DogLove2018-05-15 title Bulma article"
 
 git push -u origin master
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,24 @@
 <template>
   <section class="section">
-    <div class="hero is-danger">
+    <div class="hero is-info">
       <div calss="hero-body">
-      <h1 class="title is-1">개사랑</h1>
-      <h2 class="subtitle">개를 좋아하는 사람들</h2>
+        <div class="container">
+        <h1 class="title is-1">개사랑</h1>
+        <h2 class="subtitle">개를 좋아하는 사람들</h2>
+      </div>
     </div>
     </div>
+    <br>
+    <article class ="message">
+      <div class="message-header">개란 무엇인가?</div>
+      <div class="message-body">개는 인간의 반려 동물이다.</div>
+    </article>
 
   </section>
- </template>
+</template>
 
 
- <script>
- </script>
+<script>
+
+
+</script>
