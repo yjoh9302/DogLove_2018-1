f83ee441311232cbb4af83091b707849e600b3bd
yjoh9302 <ohyj48@naver.com>
2018-05-15 10:06:49 +0900

    DogLove2018-05-15

---
+++ commit_push.bat
@@ -0,0 +1,5 @@
+git add --all
+
+git commit -m "DogLove2018-05-15"
+
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <div class="content">
+      <h1 class="title">개사랑</h1>
+      <h2 class="subtitle">개를 좋아하는 사람들</h2>
+    </div>
+
+  </section>
+ </template>
+
+
+ <script>
+ </script>
